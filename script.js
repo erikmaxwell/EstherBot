b0c2fdb153b3d84429864005e6a083e57563639a
erikmaxwell <hello@erikmaxwell.co>
2016-08-16 14:56:56 -0700

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ module.exports = new Script({
 
     start: {
         receive: (bot) => {
-            return bot.say('So you want to learn about Esther? Just say HELLO to get started.')
+            return bot.say('Hey there, this is ErikBot, type HELLO to get started.')
                 .then(() => 'speak');
         }
     },
